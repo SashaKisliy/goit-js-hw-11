7e25dcbd6acdea427315f7becd390e78e5525eb8
SashaKisliy <108825919+SashaKisliy@users.noreply.github.com>
2022-12-14 19:09:53 +0000

    Deploying to gh-pages from @ SashaKisliy/goit-js-hw-11@595035190ff287460e69a96a7910260e01b93b2c 🚀

---
+++ index.875f3845.js
@@ -0,0 +1,2 @@
+var hello=document.querySelector(".title"),btn=document.querySelector("button");btn.addEventListener("click",(function(e){var t=e.currentTarget;console.log(t)}));
+//# sourceMappingURL=index.875f3845.js.map
